9558f29b3443841924227f2fb84dcd15889090cb
Neha <d04.neha@gmail.com>
2022-04-29 15:50:54 +0530

    return statement added in login route

--- routes/auth.js
+++ routes/auth.js
@@ -23,16 +23,22 @@ router.post("/register", async (req, res) => {
 router.post("/login", async(req,res)=>{
   try {
     const user = await User.findOne({username:req.body.username});
-    !user && res.status(400).json("Wrong Credentials!");
+    if(!user) {
+      return res.status(400).json("Wrong Credentials!");
+       
+    } 
 
     const validated = await bcrypt.compare(req.body.password,user.password);
-    !validated && res.status(400).json("Wrong Credentials!");
+    if(!validated){
+      return res.status(400).json("Wrong Credentials!");
+
+    } 
 
     const {password, ...others}= user._doc;
-    res.status(200).json(others);
+    return res.status(200).json(others);
 
   } catch (error) {
-    res.status(500).json(error);
+   return res.status(500).json(error);
   }
 });
 
